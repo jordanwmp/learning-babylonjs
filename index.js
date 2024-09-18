23ac8085865639fc7c101a53aeb34ed5bac839db
= <=>
2024-09-17 21:16:17 -0300

    start study physics. PhysicsBody and PhysicsShape!

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import {
     MeshBuilder,
     Vector3,
     PhysicsAggregate,
-    PhysicsShapeType
+    PhysicsShapeType,
+    PhysicsBody,
+    PhysicsMotionType,
+    PhysicsShapeSphere,
+    PhysicsShapeBox,
+    Quaternion
 } from '@babylonjs/core';
 
 import CreateScene from "./src/CreateScene";
@@ -16,19 +21,6 @@ await scene.renderScene()
 
 const physics = new Physics()
 //createScene.testeMesh()
-
-const sphere = new MeshBuilder.CreateSphere(
-    "sphere", { diameter: 2, segments: 32 },
-    scene.scene
-)
-
-sphere.position.y = 4
-
-const ground = new MeshBuilder.CreateGround(
-    "ground",
-    { width: 10, height: 10 },
-    scene.scene
-)
 
 // Inicialize o Havok e configure a física
 const initializePhysics = async () => {
@@ -46,6 +38,16 @@ const initializePhysics = async () => {
 
 await initializePhysics();
 
+/*const sphere = new MeshBuilder.CreateSphere(
+
+    "sphere", { diameter: 2, segments: 32 },
+    scene.scene
+)
+
+sphere.position.y = 4
+
+
+
 // Adicione os objetos de física após a inicialização do plugin
 const sphereAggregate = new PhysicsAggregate(
     sphere,
@@ -56,11 +58,76 @@ const sphereAggregate = new PhysicsAggregate(
     scene.scene
 );
 
-const groundAggregate = new PhysicsAggregate(
+*/
+
+const ground = new MeshBuilder.CreateGround(
+    "ground",
+    { width: 10, height: 10 },
+    scene.scene
+)
+
+/*const groundAggregate = new PhysicsAggregate(
     ground,
     PhysicsShapeType.BOX,
     {
-        mass: 0
+        mass: 0,
+        restitution: 1
     },
     scene.scene
+);*/
+
+const groundBody = new PhysicsBody(
+    ground,
+    PhysicsShapeType.STATIC,
+    true, 
+    scene.scene
+)
+
+const groundMaterial = {friction: 0.2, restitution: 0.3};
+
+const groundShape = new PhysicsShapeBox(
+    new Vector3(0, 0, 0),
+    Quaternion.Identity(),
+    new Vector3(10, 0.1, 10),
+    scene.scene
 );
+
+groundShape.material = (groundMaterial);
+groundBody.shape = (groundShape);
+groundBody.setMassProperties({
+    mass: 0,
+});
+
+groundBody.setMassProperties({
+    mass: 1,
+    restitution: 0
+})
+
+const sphere = new MeshBuilder.CreateSphere("sphere")
+sphere.position.y = 3
+
+//sphere.thinInstanceAddSelf()
+
+const sphereBody = new PhysicsBody(
+    sphere, 
+    PhysicsMotionType.DYNAMIC,
+    false, 
+    scene.scene
+)
+
+sphereBody.setMassProperties({
+    mass: 1
+})
+
+/*sphereBody.applyForce(
+    new Vector3(5, 0, 0),
+    new Vector3(0,0,0)
+)*/
+
+const shape = new PhysicsShapeSphere(
+    new Vector3(0,0,0),
+    1,
+    scene.scene
+)
+
+sphereBody.shape = shape
